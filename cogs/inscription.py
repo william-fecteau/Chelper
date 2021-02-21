303deff1146ed3391695e5398ed50d961ccddc9e
William Fecteau <wfecteau@edu.cegepgarneau.ca>
2021-02-20 19:57:49 -0500

    Added inscription verification with hardcoded user

--- cogs/inscription.py
+++ cogs/inscription.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import utils
 
 class Inscription(commands.Cog):
     def __init__(self, bot, dicGuilds):
@@ -7,4 +8,31 @@ class Inscription(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        print("yaho")
+        cVerification = utils.get(member.guild.categories, name="Verification")
+        tcVerification = utils.get(cVerification.text_channels, name="verification")
+
+        await tcVerification.send("To verifiy your identity, please enter your ID number in this chat (Don't worry, nobody will see it ;) )")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.author.bot:
+            cVerification = utils.get(message.guild.categories, name="Verification")
+            tcVerification = utils.get(cVerification.text_channels, name="verification")
+
+            
+            if message.channel == tcVerification:
+                msg = "This is not a valid id"
+                if message.content in self.dicGuilds[message.guild.id]["students"]:
+                    student = self.dicGuilds[message.guild.id]["students"][message.content]
+
+                    await message.author.edit(nick="{} {}".format(student["firstName"], student["lastName"]))
+                    msg = "Hi {} {}! Your account is now validated".format(student["firstName"], student["lastName"])
+
+                # TODO: Remove comment
+                # delete self.dicGuilds[message.guild.id]["students"][message.content]
+                await message.delete()
+                await tcVerification.send(msg)
+
+
+
+    
